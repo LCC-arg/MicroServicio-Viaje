9033336b02e3af568321619472f5982b088f83ad
Gonzalo Corsiglia <gonza.corsiglia@gmail.com>
2023-05-14 20:39:21 -0300

    Se borro atributo ServicioId de tabla viaje

---
+++ MicroSerivcio-Viaje/Microservicio Viaje/Application/Request/ViajeRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Request
+{
+    public class ViajeRequest
+    {
+        public int ciudadOrigen { get; set; }
+        public int ciudadDestino { get; set; }
+        public int transporteId { get; set; }
+        public string duracion { get; set; }
+        public DateTime horarioSalida { get; set; }
+        public DateTime horarioLlegada { get; set; }
+        public DateTime fechaLlegada { get; set; }
+        public DateTime fechaSalida { get; set; }
+        public string tipoViaje { get; set; }
+    }
+}
---
+++ MicroSerivcio-Viaje/Microservicio Viaje/Domain/Entities/Viaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Viaje
+    {
+        public int ViajeId { get; set; }
+        public int CiudadOrigen { get; set; }
+        public int CiudadDestino { get; set; }
+        public int TransporteId { get; set; }
+        public string Duracion { get; set; }
+        public DateTime HorarioSalida { get; set; }
+        public DateTime HorarioLlegada { get; set; }
+        public DateTime FechaLlegada { get; set; }
+        public DateTime FechaSalida { get; set; }
+        public string TipoViaje { get; set; }
+
+        public List<PasajeroViaje> PasajeroViajes { get; set; }
+       
+    }
+}
